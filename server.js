20cd01f677dc26f0955a8d04fc4c2bcf94f03989
sri-21524 <smk627751@gmail.com>
2024-08-09 11:55:42 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+const admin = require('firebase-admin');
+const serviceAccount = require('./serviceAccountKey.json');
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+});
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/send',(req,res) => {
+    console.log(req.body)
+    const{token,status} = req.body
+    const message = {
+        notification:{
+            "title": "Order Update",
+            "body": `Your order has been ${status}!`
+        },
+        token,  // Replace with the device's FCM token
+    };
+    
+    admin.messaging().send(message)
+        .then(response => {
+            console.log('Successfully sent message:', response);
+            res.send("received")
+        })
+        .catch(error => {
+            console.error('Error sending message:', error);
+            res.errored(error)
+        });
+})
+app.listen(5000)
